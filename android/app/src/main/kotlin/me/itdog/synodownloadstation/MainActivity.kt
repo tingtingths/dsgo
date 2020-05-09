86ae86d9dd4be1a2a231e958eb75b5c2be056afa
Tony Ting <k999hoshing@gmail.com>
2020-05-09 14:17:21 +0800

    Init commit

---
+++ android/app/src/main/kotlin/me/itdog/synodownloadstation/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.itdog.synodownloadstation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Welcome to Flutter',
+            home: RandomWords()
+        );
+    }
+}
+
+class RandomWords extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+        return RandomWordState();
+    }
+}
+
+class RandomWordState extends State<RandomWords> {
+    final _suggestions = <WordPair>[];
+    final _biggerFont = const TextStyle(fontSize: 18);
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Statup Name Generator'),
+            ),
+            body: _buildSuggestions(),
+        );
+    }
+
+    Widget _buildSuggestions() {
+        return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemBuilder: (context, i) {
+                if (i.isOdd) return Divider();
+
+                var index = i ~/ 2;
+                index = index % 10;
+                if (index >= _suggestions.length - 1) {
+                    var candidiates = generateWordPairs().take(10);
+                    if (_suggestions.length > 0) {
+                        _suggestions.removeRange(0, _suggestions.length);
+                    }
+                    _suggestions.addAll(candidiates);
+                    print('Gen [${candidiates.join(",")}]');
+                }
+                return _buildRow(_suggestions[index]);
+            },
+        );
+    }
+
+    Widget _buildRow(WordPair pair) {
+        return ListTile(
+            title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+            ),
+        );
+    }
+}
